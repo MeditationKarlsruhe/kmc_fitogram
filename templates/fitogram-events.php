c2b172a919ff1afd44566f467116c0f5c1c8530f
Felix Ruthenberg <felix.ruthenberg@generic.de>
2023-12-05 08:24:14 +0100

    style improvement and link to products

--- templates/fitogram-events.php
+++ templates/fitogram-events.php
@@ -43,7 +43,7 @@
                                     <?php echo $timeFormatter->format(strtotime($event->end)); ?>
                                 </span>
 
-                                <span class="mb-center maxbutton-4-center">
+                                <span class="mb-center maxbutton-4-center registration-link">
                                     <span class="maxbutton-4-container mb-container">
                                         <a class="maxbutton-4 maxbutton maxbutton-anmeldung" target="_blank"
                                             title="Anmeldung zum Kurs" rel="noopener"
@@ -73,6 +73,9 @@
 
                             </div>
                         <?php } ?>
+                        <div class="product">
+                            <a href="/treuekarten/">Zu den Details</a>
+                        </div>
                     </div>
                 </div>
             </div>
